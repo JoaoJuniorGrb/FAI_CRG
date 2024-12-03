4f5bcd86cfd28f9882c6e74cef2941fc21efaa69
Joao HF <108489218+JoaoJuniorGrb@users.noreply.github.com>
2024-12-03 20:19:51 -0300

    inventario.py

--- inventario.py
+++ inventario.py
@@ -76,7 +76,7 @@ authenticator = stauth.Authenticate(
         names=[user['name'] for user in config['credentials']['usernames'].values()],
         usernames=list(config['credentials']['usernames'].keys()),
         passwords=[user['password'] for user in config['credentials']['usernames'].values()],
-        cookie_name="test_cookie_name",  # Verifique se isso está apontando para 'random_cookie_name'
+        cookie_name="random_cookie_name",  # Verifique se isso está apontando para 'random_cookie_name'
         key=config['cookie']['key'],  # Verifique se a chave 'random_signature_key' está correta
         cookie_expiry_days=config['cookie']['expiry_days']
 
